f24a581ba27dff732585e8e1a00f9a3f7dc2dd70
denduro <denduro@users.noreply.github.com>
2025-06-07 03:32:35 +0000

    Create base Express app with Spider-Man route

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,7 @@ import { spiderManHandler } from './routes/spiderMan';
 
 const app = express();
 
+// Routes
 app.get('/spiderMan', spiderManHandler);
-app.get('/spiderman', spiderManHandler);
-app.get('/spider-man', spiderManHandler);
-app.get('/SpiderMan', spiderManHandler);
 
 export default app;
