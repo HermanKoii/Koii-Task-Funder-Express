888b37b00bef667c3f7046ee159d4fecaffb7ee5
ItsHugoo <ItsHugoo@users.noreply.github.com>
2025-06-07 00:33:31 +0000

    Add heroes data with Iron Man details

---
+++ src/data/heroes.js
@@ -0,0 +1,9 @@
+export const heroes = {
+  ironMan: {
+    name: 'Tony Stark',
+    heroName: 'Iron Man',
+    abilities: ['Genius-level intellect', 'Advanced armor technology', 'Flight'],
+    firstAppearance: 'Tales of Suspense #39 (1963)',
+    powerLevel: 9
+  }
+};
