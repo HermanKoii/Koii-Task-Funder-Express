b743c2c8b4d635e993ca7fd3c01dcfccab8f21fb
denduro <denduro@users.noreply.github.com>
2025-06-07 05:03:38 +0000

    Add Jest configuration for TypeScript and JavaScript tests

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.js$': 'babel-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testMatch: [
+    '**/__tests__/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[tj]s?(x)'
+  ],
+  transformIgnorePatterns: [
+    '/node_modules/(?!(@_koii|axios)/)' 
+  ]
+};
